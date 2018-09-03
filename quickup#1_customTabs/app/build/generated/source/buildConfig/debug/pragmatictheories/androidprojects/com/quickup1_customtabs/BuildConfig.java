28750302759bd9967517d3fd903dad03affa2fb0
AmalH <hichri.amal@hotmail.fr>
2018-09-03 16:27:55 -0400

    updated gitignore

---
+++ quickup#1_customTabs/app/build/generated/source/buildConfig/debug/pragmatictheories/androidprojects/com/quickup1_customtabs/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package pragmatictheories.androidprojects.com.quickup1_customtabs;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "pragmatictheories.androidprojects.com.quickup1_customtabs";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
